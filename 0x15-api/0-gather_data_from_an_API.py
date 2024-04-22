0f8a7a07e5b24bec530f58d811030fddac8209c0
b7sh <mohammedabdelbasit500@gmail.com>
2024-04-22 15:35:59 -0700

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    a Python script that, using this REST API,
+    for a given employee ID, returns information
+    about his/her TODO list progress.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    response = requests.get(url + "users/{}".format(id)).json()
+    todo = requests.get(url + "todos", params={"user_id": id}).json()
+
+    completed = []
+    for element in todo:
+        if element["completed"] is True:
+            completed.append(element["title"])
+        print("Employee {} is done with tasks({}/{}):"
+                .format(response.get("name"), len(completed), len(todo)))
+        for i in completed:
+            print("\t {}".format(i))
+    
