5e14bbbfe443a9c252fbaadf6a30b963592aa10b
b7sh <mohammedabdelbasit500@gmail.com>
2024-04-22 15:45:53 -0700

    depuging

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     response = requests.get(url + "users/{}".format(id)).json()
-    todo = requests.get(url + "todos", params={"user_id": id}).json()
+    todo = requests.get(url + "todos", params={"userId": id}).json()
 
     completed = []
     for element in todo:
-        if element.get("completed") is True:
-            completed.append(element.get("title"))
-        print("Employee {} is done with tasks({}/{}):"
-                .format(response.get("name"), len(completed), len(todo)))
+        if element["completed"] is True:
+            completed.append(element["title"])
+        print(
+                "Employee {} is done with tasks({}/{}):".format(
+                    response.get("name"), len(completed), len(todo)))
         for i in completed:
             print("\t {}".format(i))
-    
