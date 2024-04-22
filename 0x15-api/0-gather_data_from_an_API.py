e34227e5155713980a2b4e154a4aff578fd5dbd4
b7sh <mohammedabdelbasit500@gmail.com>
2024-04-22 16:54:23 -0700

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,14 +11,16 @@ import sys
 if __name__ == "__main__":
     employee_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    response = requests.get(url + "users/{}".format(employee_id))
+    user = response.json()
     # .json() converts json string in to a dictionary
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    todos_response = requests.get(url + "todos", params={"userId": employee_id})
+    todos = todos_response.json()
     completed = []
     for todo in todos:
         if todo.get("completed") is True:
             completed.append(todo.get("title"))
-    print("Employee{}is done with tasks({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todo)))
     for index in completed:
-        print("\t{}".format(index))
+        print("\t {}".format(index))
