333cc23db402560c8b8554f29c44fb3fa0397b79
b7sh <mohammedabdelbasit500@gmail.com>
2024-08-14 05:58:15 +0000

    modify-6

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,11 +7,11 @@ def top_ten(subreddit):
     '''If not a valid subreddit, print None.'''
     url = ('https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit))
     response = requests.get(url, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code == 400:
+        print('None')
+    else:
         data = response.json()
         posts = (data['data']['children'])
         for post in posts:
             title = post['data']['title']
             print(title)
-    else:
-        print('None')
