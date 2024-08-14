6b2d221c506747409c1b8101e3847196e7537833
b7sh <mohammedabdelbasit500@gmail.com>
2024-08-14 05:34:16 +0000

    modify-2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Function to queries the Reddit API and returns the number of subscribers"""
+"""Returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url,
-                             headers=headers,
-                               allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+	"""Return the total number of subscribers."""
+	url = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+	response = requests.get(url, allow_redirects=False)
+	if response.status_code == 200:
+		data = response.josn()
+		return data['data']["subscribers"]
+	else:
+		return 0
