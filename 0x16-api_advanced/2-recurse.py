8b2ddfd6af2fd18f47f89a6e74f8a64a28ed96aa
b7sh <mohammedabdelbasit500@gmail.com>
2024-08-14 06:00:24 +0000

    modify-8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,9 @@ def recurse(subreddit, hot_list=[], after_value=None):
     '''If not a valid subreddit, print None.'''
     url = ('https://www.reddit.com/r/{}/hot.json'.format(subreddit))
     response = requests.get(url, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code == 400:
+        return None
+    else:
         data = response.json()
         posts = (data['data']['children'])
         for post in posts:
@@ -18,5 +20,3 @@ def recurse(subreddit, hot_list=[], after_value=None):
             return recurse(subreddit, hot_list, after_value)
         else:
             return hot_list
-    else:
-        return None
