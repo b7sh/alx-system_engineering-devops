aa94dd303eae9a36601b790bac08ec2314398b68
b7sh <mohammedabdelbasit500@gmail.com>
2024-08-14 05:51:33 +0000

    modify-2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''If not a valid subreddit, return 0'''
-    url = (
-          'https://www.reddit.com/r/{}/about.json'.format(
-                                                         subreddit))
-    response = requests.get(url,
-allow_redirects=False)
+    url = ('https://www.reddit.com/r/{}/about.json'.format(subreddit))
+    response = requests.get(
+			url, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return (data['data']['subscribers'])
     else:
-        return 0
+        return (0)
