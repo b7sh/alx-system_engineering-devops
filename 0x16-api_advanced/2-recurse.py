ab3544ee379259e9214d7bd6b8bd027e31323791
b7sh <mohammedabdelbasit500@gmail.com>
2024-08-14 06:01:46 +0000

    modify-9

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,8 +8,6 @@ def recurse(subreddit, hot_list=[], after_value=None):
     url = ('https://www.reddit.com/r/{}/hot.json'.format(subreddit))
     response = requests.get(url, allow_redirects=False)
     if response.status_code == 400:
-        return None
-    else:
         data = response.json()
         posts = (data['data']['children'])
         for post in posts:
@@ -20,3 +18,5 @@ def recurse(subreddit, hot_list=[], after_value=None):
             return recurse(subreddit, hot_list, after_value)
         else:
             return hot_list
+    else:
+        return (None)
