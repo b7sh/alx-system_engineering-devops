e41db51e7ac49b98830ad6116dbd44e40cc30857
b7sh <mohammedabdelbasit500@gmail.com>
2024-08-14 05:52:06 +0000

    modify-2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     '''If not a valid subreddit, return 0'''
     url = ('https://www.reddit.com/r/{}/about.json'.format(subreddit))
-    response = requests.get(
-			url, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return (data['data']['subscribers'])
